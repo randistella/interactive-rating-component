42f1a89785e2b975e47960927ae79b9745f3352c
Randi Stella <RANDINICOLESTELLA@GMAIL.COM>
2023-03-04 11:40:06 -0600

    project finished

--- dist/app.js
+++ dist/app.js
@@ -4,11 +4,15 @@ const submitBtn = document.querySelector('.sub-btn');
 const rating = document.getElementById('rating');
 const rates = document.querySelectorAll('.rt-btn')
 
+/* when user clicks, removes hide css class from thank you box
+and adds hide class to main box, making it disappear*/
 submitBtn.addEventListener('click', () => {
     thankyouBox.classList.remove('hide');
     mainBox.classList.add('hide');
 });
 
+/* loops through each rating, when user clicks on one
+adds to HTML span */
 rates.forEach((rate) => {
     rate.addEventListener('click', () =>
     rating.innerHTML = rate.innerHTML);
